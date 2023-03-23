2fe23f30d68e086acc5ac9cca974adef266d45c3
Kimtaehoon99 <2019010391@gnu.ac.kr>
2023-03-23 15:30:54 +0900

    메뉴수

--- src/management/Management.java
+++ src/management/Management.java
@@ -5,13 +5,10 @@ public class Management {
 	public static void main(String[] args) {
 		System.out.println("Employee Management System");
 		System.out.println("1. View Employee list");
-		System.out.println("2. View Employee details");
-		System.out.println("3. Manage Employee Attendance");
-		System.out.println("4. Manage Employee Salary");
-		System.out.println("5. Update Employee Information");
-		System.out.println("6.Add New Employee");
-		System.out.println("Remove Employee");
-		System.out.println("Exit");
+		System.out.println("2. Manage Employee Attendance");
+		System.out.println("3. Manage Employee Salary");
+		System.out.println("4. Update Employee Information");
+		System.out.println("5. Exit");
 
 	}
 
