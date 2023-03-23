0f59d18e6a0fdf54693df88e8feb3f3d27e56010
Kimtaehoon99 <2019010391@gnu.ac.kr>
2023-03-23 14:50:48 +0900

    fix menu

--- src/management/Management.java
+++ src/management/Management.java
@@ -3,7 +3,15 @@ package management;
 public class Management {
 
 	public static void main(String[] args) {
-		System.out.print("Github Test!!");
+		System.out.println("Employee Management System");
+		System.out.println("1. View Employee list");
+		System.out.println("2. View Employee details");
+		System.out.println("3. Manage Employee Attendance");
+		System.out.println("4. Manage Employee Salary");
+		System.out.println("5. Update Employee Information");
+		System.out.println("6.Add New Employee");
+		System.out.println("Remove Employee");
+		System.out.println("Exit");
 
 	}
 
