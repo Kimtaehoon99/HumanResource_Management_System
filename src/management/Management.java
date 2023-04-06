e204f3b1e8d373e454b0b93088a1fd2583ed2ca2
k74035 <126762577+k74035@users.noreply.github.com>
2023-04-06 14:46:47 +0900

    Update Management.java

--- src/management/Management.java
+++ src/management/Management.java
@@ -6,48 +6,92 @@ public class Management {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+		int number = 6;
 		
 		for(;;){
 			
-			System.out.println("Employee Management System");
+			System.out.println("***Employee Management System***");
 			System.out.println("1. View Employee list");
 			System.out.println("2. Add Employee");
-			System.out.println("3. Manage Employee Salary");
-			System.out.println("4. Update Employee Information");
-			System.out.println("5. Exit");
+			System.out.println("3. Delete Employee");
+			System.out.println("4. Manage Employee Salary");
+			System.out.println("5. Update Employee Information");
+			System.out.println("6. Exit");
 			System.out.print("\n");
 			System.out.print("Select options: ");
 			
-			Scanner input=new Scanner(System.in);
-			
-			int number=input.nextInt();
-			
+			number = input.nextInt();
 			switch(number) {
-			case 1:
-				System.out.println("List");
-				System.out.println("1.Kim");
-				System.out.println("2.Park");
-				System.out.println("3.Lee");
+			case 1: {
+				System.out.println("<View list>");
+				viewList();
 				break;
-			case 2:
-				System.out.println("Add menu");
+			}
+			case 2: {
+				System.out.println("<Add menu>");
+				addEmployee();
 				break;
-			case 3:
-				System.out.println("Salary menu");
+			}
+			case 3: {
+				System.out.println("<Delete menu>");
+				deleteEmployee();
 				break;
-			case 4:
-				System.out.println("Update menu");
+			}
+			case 4: {
+				System.out.println("<Salary menu>");
+			}
 				break;
-			case 5:
-				System.out.println("return 0");
+			case 5: {
+				System.out.println("<Update menu>");
+				updateEmployee();
 				break;
+			}
+			case 6:
+				return;
 			default: 
 				System.out.println("Wrong option! Select options again!! ");
 				System.out.println("\n");
 				break;
 			}
-			if(number>=1 && number<=5)
-				break;
+			System.out.println("\n");		
 		}
+	}
+	
+	public static void viewList() {
+		System.out.println("1.Kim");
+		System.out.println("2.Park");
+		System.out.println("3.Lee");
+	}
+	
+	
+	public static void addEmployee() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Employee name:");
+		String employeeName = input.next();
+		System.out.print("Employee rank:");
+		String employeeRank = input.next();
+		System.out.print("PhoneNumber:");
+		int PhoneNumber = input.nextInt();
+		System.out.print("Employee Email:");
+		String employeeEmail = input.next();
+	}
+	public static void deleteEmployee() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Employee name:");
+		String employeeName = input.next();
+		
+	}
+	public static void salaryMenu() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Employee name:");
+		String employeeName = input.next();
+		
+	}
+	public static void updateEmployee() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Employee name:");
+		String employeeName = input.next();
+		
 	}
 }
